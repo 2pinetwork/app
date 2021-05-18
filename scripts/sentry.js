fc6c4482f0842496412a4d8f5d4f2dc532cad277
Batudo <batudo@2pi.finance>
2021-05-18 20:58:03 -0300

    Sentry release + source maps

---
+++ scripts/sentry.js
@@ -0,0 +1,32 @@
+const SentryCli = require('@sentry/cli')
+
+const createRelease = async () => {
+  const cli     = new SentryCli()
+  const release = process.env.REACT_APP_SENTRY_RELEASE
+
+  if (! release) {
+    console.warn('REACT_APP_SENTRY_RELEASE is not set')
+    return
+  }
+
+  try {
+    const options = {
+      include:   ['build/static/js'],
+      urlPrefix: '~/static/js',
+      rewrite:   false
+    }
+
+    console.log(`Creating sentry release ${release}`)
+    await cli.releases.new(release)
+
+    console.log('Uploading source maps')
+    await cli.releases.uploadSourceMaps(release, options)
+
+    console.log('Finalizing release')
+    await cli.releases.finalize(release)
+  } catch (e) {
+    console.error('Source maps uploading failed:', e)
+  }
+}
+
+createRelease()
--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ import { dsn, integrations } from './data/sentry'
 import App from './components/app'
 import './css/app.scss'
 
-Sentry.init({ dsn, integrations, tracesSampleRate: 1.0 })
+const release = process.env.REACT_APP_SENTRY_RELEASE
+
+if (release) {
+  Sentry.init({ dsn, release, integrations, tracesSampleRate: 1.0 })
+}
 
 ReactDOM.render(
   <React.StrictMode>
