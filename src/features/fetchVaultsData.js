0bbfddae03af7fe1b5b3717ae75ea4dfe564958c
Batudo <82539708+batudo@users.noreply.github.com>
2021-08-05 12:46:04 -0300

    New manual Curve BTC APY.

--- src/features/fetchVaultsData.js
+++ src/features/fetchVaultsData.js
@@ -19,7 +19,7 @@ const helpers = {
   let altApy = 0
 
     if (vault.pool === 'curve') {
-      apy = 0.0941 // To be really calculated
+      apy = 0.1008 // To be really calculated
     } else {
       apy    = getVaultApy(vault, dataProvider, distributionManager, prices)
       altApy = getVaultApy(vault, dataProvider, distributionManager, prices, 0)
