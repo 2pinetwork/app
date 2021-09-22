b14735d42db9b408d55d37fa1da706e6dbb04231
Matías Galí <matiasxgali@gmail.com>
2021-09-22 19:20:52 -0300

    Fix compounding when calculating APYs.

--- src/helpers/apy.js
+++ src/helpers/apy.js
@@ -1,12 +1,15 @@
 const BigNumber = require('bignumber.js')
 
 const SECONDS_PER_YEAR = 31536000
-const BASE_HPY = 8760
-const RAY_DECIMALS = '1e27'
-const COMMON_DECIMALS = '1e18'
+const BASE_HPY         = 8760
+const RAY_DECIMALS     = '1e27'
+const COMMON_DECIMALS  = '1e18'
+const PERFORMANCE_FEE  = 0.035
 
-const compound = (r, n = 365, t = 1, c = 1) => {
-  return (1 + (r * c) / n) ** (n * t) - 1
+// Convert annual proportional rates (APR) to annual compound rates (APY).
+// Takes the rate and the amount of compoundings per year.
+const toCompoundRate = (r, n = 365) => {
+  return (1 + r / n) ** n - 1
 }
 
 const getVaultApy = (vault, dataProvider, distributionManager, prices, depth) => {
@@ -34,8 +37,9 @@ const getVaultApy = (vault, dataProvider, distributionManager, prices, depth) =>
     vault.borrow.percentage
   )
 
-  let totalMatic = leveragedSupplyMatic.plus(leveragedBorrowMatic)
-  let compoundedMatic = compound(totalMatic, BASE_HPY, 0.96)
+  const totalMatic          = leveragedSupplyMatic.plus(leveragedBorrowMatic)
+  const totalMaticAfterFees = totalMatic * (1 - PERFORMANCE_FEE)
+  const compoundedMatic     = toCompoundRate(totalMaticAfterFees, BASE_HPY)
 
   return leveragedSupplyBase.minus(leveragedBorrowBase).plus(compoundedMatic).toNumber()
 }
